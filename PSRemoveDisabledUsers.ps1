924531d9cf7289ed986bf1bd362962abb6e4b9b1
nmingo <nicholas.mingo@gmail.com>
2021-01-14 14:59:34 -0600

    Initial commit

---
+++ PSRemoveDisabledUsers.ps1
@@ -0,0 +1,16 @@
+﻿$search = New-Object System.DirectoryServices.DirectorySearcher
+$search.SearchRoot = [ADSI]"LDAP://DC=contoso,DC=com"
+
+$exclude = @('NetworkService','LocalService','systemprofile')
+$profiles = Get-WmiObject -ClassName Win32_UserProfile | Where-Object {$_.LocalPath.split('\')[-1] -notin $exclude}
+
+foreach ($p in $profiles) {
+    $user = $profile.LocalPath.Split('\')[-1]
+    $filter = "(&(objectCategory=person)(objectClass=user)(anr=$($user)))"
+    $search.Filter = $filter
+    $results = $search.FindOne()
+
+    if ($results.Properties.useraccountcontrol -eq 514) {
+        $p.Delete()
+    }
+}
