b4b8d5a0597f51b1e31b39a10307d7a27198dd17
Nicholas Mingo <nicholas.mingo@gmail.com>
2022-04-20 15:26:46 -0500

    Renamed script and fixed bug

--- Remove-DisabledUsers.ps1
+++ Remove-DisabledUsers.ps1
@@ -1,11 +1,11 @@
 ﻿$search = New-Object System.DirectoryServices.DirectorySearcher
-$search.SearchRoot = [ADSI]"LDAP://DC=contoso,DC=com"
+$search.SearchRoot = [ADSI]"LDAP://DC=domain,DC=com"
 
 $exclude = @('NetworkService','LocalService','systemprofile')
 $profiles = Get-WmiObject -ClassName Win32_UserProfile | Where-Object {$_.LocalPath.split('\')[-1] -notin $exclude}
 
 foreach ($p in $profiles) {
-    $user = $profile.LocalPath.Split('\')[-1]
+    $user = $p.LocalPath.Split('\')[-1]
     $filter = "(&(objectCategory=person)(objectClass=user)(anr=$($user)))"
     $search.Filter = $filter
     $results = $search.FindOne()
